1faf487d65328f457b1443deb5d2c21768a89488
QuangTrung <trungnqth1809019@fpt.edu.vn>
2018-10-10 20:42:06 +0700

    BaiTap3

---
+++ cmake-build-debug/CMakeFiles/A20181010BaiTap3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/A20181010BaiTap3.dir/main.c.obj"
+  "A20181010BaiTap3.pdb"
+  "A20181010BaiTap3.exe"
+  "A20181010BaiTap3.exe.manifest"
+  "libA20181010BaiTap3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/A20181010BaiTap3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int findMaxValue(int array[], int ia) {
+    int max = array[0];
+    for (int i = 0; i < ia; i++) {
+        if (max < *(array + i)) {
+            max = *(array + i);
+        }
+    }
+    return max;
+}
+
+int findMinValue(int array[], int ia) {
+    int min = array[0];
+    for (int i = 0; i < ia; i++) {
+        if (min > *(array + i)) {
+            min = *(array + i);
+        }
+    }
+    return min;
+}
+
+int main() {
+    int ia, ilc;
+    printf("nhap vao so luong phan tu(so phan tu phai nho hon hoac bang 10)\n");
+    scanf("%d", &ia);
+    int array[ia];
+    for (int i = 0; i < ia; i++) {
+        printf("nhap vao phan tu thu %d:\n", i + 1);
+        scanf("%d", array + i);
+    }
+    printf("\ncac phan tu trong mang la\n");
+    for (int i = 0; i < ia; i++) {
+        printf("phan tu thu %d la: %d\n", i + 1, *(array + i));
+    }
+    printf("chon phep tinh\n");
+    printf("1. tim so lon nhat\n");
+    printf("2. tim so nho nhat\n");
+    printf("lua chon cua ban 1/2\n");
+    scanf("%d", &ilc);
+    switch (ilc) {
+        case 1:
+            printf("so lon nhat la %d\n", findMaxValue(array, ia));
+            break;
+        case 2:
+            printf("so be nhat la %d\n", findMinValue(array, ia));
+            break;
+    }
+    printf("tam biet\n");
+    return 0;
+}
